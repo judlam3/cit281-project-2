037f71063039abdda118f56ef935a080b1a4ccd8
Judy Lam <judyl@uoregon.edu>
2020-04-22 23:15:16 -0700

    Project 2 functions.js

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,36 @@
 // jshint esversion: 6
 
+//Req. 3a
+let revWords1 = (str) => {
+  var revW1 = "";
+  var rev1 = str.split("");
+  for (var i = rev1.length - 1; i >= 0; i--){
+    revW1 += rev1[i];
+  }
+  return revW1;
+};
+
+//Req. 3b
+let revWords2 = (str) => {
+  var rev2 = "";
+var arr = str.split("");
+arr.reverse().forEach(function(rev2) {
+console.log(rev2);
+});
+};
+
+//Req. 3c
+let revWords3 = (str) => {
+  var revW3 = "";
+  var rev3 = str.split("");
+  var rev33 = rev3.reverse();
+  for (var x of rev33){
+    revW3 = revW3 + x;
+  }
+  return revW3;
+};
+
+//XC
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
